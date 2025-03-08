40e454ff87870fe0a245edc2a259e7bb40f375d9
BliAli <aliaddamawi@gmail.com>
2025-03-08 09:51:47 +0700

    Mmebuat Data Member, Constructor, serta Function mengembalikan data rental

--- src/rentalsystem/DataRental.java
+++ src/rentalsystem/DataRental.java
@@ -2,5 +2,32 @@ package rentalsystem;
 
 /* @author aliad */
 public class DataRental {
+    private String namaPenyewa, jenisKendaraan, namaKendaraan;
+    private int noTelpon, lamaSewa;
+    private double hargaSewa, totalHargaSewa;
+
+    public DataRental(String nama, String jenis, String namaKendaraan, int noTelp, int lamaSewa, double harga) {
+        this.namaPenyewa = nama;
+        this.jenisKendaraan = jenis;
+        this.noTelpon = noTelp;
+        this.lamaSewa = lamaSewa;
+        this.totalHargaSewa = harga;
+        this.namaKendaraan = namaKendaraan;
+    }
+
+    public double getTotalHargaSewa() {
+        return this.hargaSewa * this.lamaSewa;
+    }
     
+    
+    
+    public String getDetail(){
+        return 
+            "Nama\t\t: " + this.namaPenyewa +
+            "\nNo Telepon\t: " + this.noTelpon +
+            "\nJenis Kendaraan\t: " + this.jenisKendaraan +
+            "\nNama Kendaraan\t: " + this.namaKendaraan +
+            "\nLama Sewa\t: " + this.lamaSewa + " Hari" +
+            "\nTotal Harga\t: " + this.getTotalHargaSewa();
+    }
 }
