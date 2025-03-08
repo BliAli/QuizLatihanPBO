318fd8d4295f0819e6e436d27f85b394e2c8238f
BliAli <aliaddamawi@gmail.com>
2025-03-08 11:19:53 +0700

    Mengahapus fungsi serta data totalHargaSewa

--- src/rentalsystem/DataRental.java
+++ src/rentalsystem/DataRental.java
@@ -4,21 +4,16 @@ package rentalsystem;
 public class DataRental {
     private String namaPenyewa, jenisKendaraan, namaKendaraan;
     private int noTelpon, lamaSewa;
-    private double hargaSewa, totalHargaSewa;
+    private double hargaSewa;
 
     public DataRental(String nama, String jenis, String namaKendaraan, int noTelp, int lamaSewa, double harga) {
         this.namaPenyewa = nama;
         this.jenisKendaraan = jenis;
         this.noTelpon = noTelp;
         this.lamaSewa = lamaSewa;
-        this.totalHargaSewa = harga;
+        this.hargaSewa = harga;
         this.namaKendaraan = namaKendaraan;
     }
-
-    public double getTotalHargaSewa() {
-        return this.hargaSewa * this.lamaSewa;
-    }
-    
     
     
     public String getDetail(){
@@ -27,7 +22,6 @@ public class DataRental {
             "\nNo Telepon\t: " + this.noTelpon +
             "\nJenis Kendaraan\t: " + this.jenisKendaraan +
             "\nNama Kendaraan\t: " + this.namaKendaraan +
-            "\nLama Sewa\t: " + this.lamaSewa + " Hari" +
-            "\nTotal Harga\t: " + this.getTotalHargaSewa();
+            "\nLama Sewa\t: " + this.lamaSewa + " Hari";
     }
 }
