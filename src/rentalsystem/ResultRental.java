2894cb742dc52101ec43a2abb184c06742c5fde6
BliAli <aliaddamawi@gmail.com>
2025-03-08 11:33:46 +0700

    Result sementara

--- src/rentalsystem/ResultRental.java
+++ src/rentalsystem/ResultRental.java
@@ -1,13 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package rentalsystem;
 
-/**
- *
- * @author aliad
- */
-public class ResultRental {
+import java.awt.HeadlessException;
+import java.awt.ScrollPane;
+import javax.swing.*;
+
+/* @author aliad */
+public class ResultRental extends JFrame {
+
+    public ResultRental(DataRental data) throws HeadlessException {
+        setTitle("Result Penyewaan");
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        
+        JTextArea resultData = new JTextArea(data.getDetail());
+        resultData.setEditable(false);
+        add(new JScrollPane(resultData));
+        
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
     
 }
