6e6f728891a5b7ff1a3cec4a1ff1b2b3fa0b92e4
BliAli <aliaddamawi@gmail.com>
2025-03-08 10:03:54 +0700

    Mmebuat fungsi ketika button beserta parameternya

--- src/rentalsystem/RentalSystemGUI.java
+++ src/rentalsystem/RentalSystemGUI.java
@@ -1,13 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package rentalsystem;
 
-/**
- *
- * @author aliad
- */
-public class RentalSystemGUI {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/* @author aliad */
+public class RentalSystemGUI extends JFrame implements ActionListener{
+    private JLabel labelPilihan;
+    private JButton btnMotor, btnMobil;
+    
+    public RentalSystemGUI() throws HeadlessException {
+        setTitle("Rental System by Bli Ali");
+        setLayout(new GridLayout(2, 1));
+        setSize(400, 300);
+        
+        labelPilihan = new JLabel("Pilih Jenis Kendaraan Yang Ingin Anda Sewa : ", SwingConstants.CENTER);
+        add(labelPilihan);
+        
+        JPanel btnKendaraan = new JPanel(new GridLayout(1, 2, 10, 0));
+        btnMotor = new JButton("Motor");
+        btnMobil = new JButton("Mobil");
+        
+        btnKendaraan.add(btnMotor);
+        btnKendaraan.add(btnMobil);
+        
+        add(btnKendaraan);
+        
+        btnMotor.addActionListener(this);
+        btnMobil.addActionListener(this);
+        
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == btnMotor) {
+            new FormPenyewaan("Motor");
+        } else if (e.getSource() == btnMobil){
+            new FormPenyewaan("Mobil");
+        }
+    }
+    
     
 }
