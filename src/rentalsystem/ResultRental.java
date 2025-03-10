b913257879eb8386e1b282a9d59df41bbdf4b42c
BliAli <aliaddamawi@gmail.com>
2025-03-10 15:04:50 +0700

    Merapihkan tampilan dengan border layout, sehingga dapat menambahkan button dibawah panel. Dan menambahkan button kemabli ke form

--- src/rentalsystem/ResultRental.java
+++ src/rentalsystem/ResultRental.java
@@ -1,23 +1,57 @@
 package rentalsystem;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.HeadlessException;
 import java.awt.ScrollPane;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 /* @author aliad */
-public class ResultRental extends JFrame {
-
-    public ResultRental(DataRental data) throws HeadlessException {
+public class ResultRental extends JFrame implements ActionListener{
+    JButton btnKembali, btnDone;
+    JFrame prevPage;
+    public ResultRental(DataRental data, JFrame prevPage) throws HeadlessException {
+        this.prevPage = prevPage;
         setTitle("Result Penyewaan");
+        setLayout(new BorderLayout(10, 10));
         setSize(400, 300);
         setLocationRelativeTo(null);
         
         JTextArea resultData = new JTextArea(data.getDetail());
         resultData.setEditable(false);
-        add(new JScrollPane(resultData));
+        add(new JScrollPane(resultData), BorderLayout.CENTER);
+        
+        JPanel panelBawah = new JPanel(new BorderLayout());
+        JPanel panelKiri = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        btnKembali = new JButton("Kembali");
+        btnKembali.addActionListener(this);
+        panelKiri.add(btnKembali);
+        panelBawah.add(panelKiri, BorderLayout.WEST);
+        
+        JPanel panelKanan = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        btnDone = new JButton("Done");
+        btnDone.addActionListener(this);
+        panelKanan.add(btnDone);
+        panelBawah.add(panelKanan, BorderLayout.EAST);
+        
+        add(panelBawah, BorderLayout.SOUTH);
         
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == btnKembali) {
+            dispose();
+            prevPage.setVisible(true);
+        } else if (e.getSource() == btnDone){
+            System.exit(0);
+        }
+    }
+    
+    
     
 }
